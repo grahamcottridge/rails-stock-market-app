37608a1e23886eb3ba7e2400807ead6ecc9b576b
Graham Cottridge <codinggc@gmail.com>
2018-04-09 10:45:24 +0100

    Add bootstrap styles to stock list table

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,28 +3,20 @@ class StocksController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
 
-  # GET /stocks
-  # GET /stocks.json
   def index
     @stocks = Stock.all
   end
 
-  # GET /stocks/1
-  # GET /stocks/1.json
   def show
   end
 
-  # GET /stocks/new
   def new
     @stock = Stock.new
   end
 
-  # GET /stocks/1/edit
   def edit
   end
 
-  # POST /stocks
-  # POST /stocks.json
   def create
     @stock = Stock.new(stock_params)
 
@@ -39,8 +31,6 @@ class StocksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /stocks/1
-  # PATCH/PUT /stocks/1.json
   def update
     respond_to do |format|
       if @stock.update(stock_params)
@@ -53,8 +43,6 @@ class StocksController < ApplicationController
     end
   end
 
-  # DELETE /stocks/1
-  # DELETE /stocks/1.json
   def destroy
     @stock.destroy
     respond_to do |format|
